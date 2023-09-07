4612602de52769a03b83eb86911c5a26eac769ad
pankaj1980patel <pantherpatel1980@gmail.com>
2023-09-07 13:22:53 +0530

    Added Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,41 +1,35 @@
 // import logo from './logo.svg';
-import React from 'react';
-import { Route,BrowserRouter as Router, Switch, Redirect } from 'react-router-dom/cjs/react-router-dom.min';
-import './App.css';
+import React from "react";
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+} from "react-router-dom/cjs/react-router-dom.min";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
-import Users from './user/pages/Users';
+import "./App.css";
+
+import Users from "./user/pages/Users";
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <Router>
-      <Switch>
-        <Route path='/' exact >
-          <h2>hell</h2>
-        </Route>
-        <Route path='/hello' exact>
-          <h1>chutp chuitye</h1>
-        </Route>
-        <Route path='/pa' exact>
-          <Users/>
-        </Route>
-        <Redirect to='/'/>
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <h2>hell</h2>
+          </Route>
+          <Route path="/hello" exact>
+            <h1>chutp chuitye</h1>
+          </Route>
+          <Route path="/pa" exact>
+            <Users />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 }
