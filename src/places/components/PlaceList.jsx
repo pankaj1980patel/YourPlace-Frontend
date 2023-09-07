060e6cac05be84afaaf6edb7ed1b9822995bb74d
pankaj1980patel <pantherpatel1980@gmail.com>
2023-09-07 19:08:49 +0530

    Start to add Places

---
+++ src/places/components/PlaceList.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import Card from "../../shared/components/UIElements/Card";
+import PlaceItem from "./PlaceItem";
+import "./PlaceList.css";
+
+const PlaceList = (props) => {
+  if (props.item.length === 0) {
+    return (
+      <div className="place-list center">
+        return (
+        <Card>
+          <h2>No places found maybe create one?</h2>
+          <button>Share Place</button>
+        </Card>
+        )
+      </div>
+    );
+  }
+  return (
+    <ul className="place-list">
+      {props.items.map((place) => {
+        <PlaceItem key={place.id} id={place.id} image={place.imageUrl} title={place.title} description={place.description} address={place.address} 
+                creatorId={place.creatorId} />;
+      })}
+    </ul>
+  );
+};
+
+export default PlaceList;
